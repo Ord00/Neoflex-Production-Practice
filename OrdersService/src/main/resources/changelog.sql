0a1ba29ac118778950c858d98d32b8bdbb54b2d9
Ord00 <ivansimonov7@yandex.ru>
2026-04-22 14:13:42 +0300

    Removed changelog.sql

--- OrdersService/src/main/resources/changelog.sql
+++
@@ -1,8 +0,0 @@
-create table orders
-(
-    id_order uuid primary key,
-    id_product varchar(36) not null,
-    count integer not null,
-    id_user varchar(36) not null,
-    date_create timestamp not null default now()
-);
