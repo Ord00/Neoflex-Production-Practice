43384cb603edd03b72b8204a3ad9f6326de823c4
Ord00 <ivansimonov7@yandex.ru>
2026-04-22 15:07:35 +0300

    Refactored logs

--- ProductService/src/main/java/neoflex/prod/practice/services/ProductsService.java
+++ ProductService/src/main/java/neoflex/prod/practice/services/ProductsService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import neoflex.prod.practice.entities.ProductsEntity;
 import neoflex.prod.practice.repositories.ProductsRepository;
-import org.slf4j.event.Level;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +18,7 @@ public class ProductsService {
 
     @Transactional
     public void reserveProduct(UUID idProduct, int count) {
-        log.atLevel(Level.INFO).log("Поиск продукта для резервирования c idProduct = {}", idProduct);
+        log.info("Поиск продукта для резервирования c idProduct = {}", idProduct);
 
         Optional<ProductsEntity> productsEntity = productsRepository.findById(idProduct);
         if (productsEntity.isPresent()) {
@@ -27,14 +26,14 @@ public class ProductsService {
             countUpdate(product, count);
             productsRepository.save(product);
         } else {
-            log.atLevel(Level.ERROR).log("Продукт с idProduct = {} не найден", idProduct);
+            log.error("Продукт с idProduct = {} не найден", idProduct);
         }
     }
 
     private void countUpdate(ProductsEntity product, int reserveCount) {
         int oldCount = product.getCount();
         int newCount = oldCount - reserveCount;
-        log.atLevel(Level.INFO).log("Изменение count продукта (idProduct = {}) c {}, на {}",
+        log.info("Изменение count продукта (idProduct = {}) c {}, на {}",
                 product.getId(), oldCount, newCount);
         product.setCount(newCount);
     }
